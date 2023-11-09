58e4517bc5674a6366435b30e6c25ca26bcac11a
Dor-sketch <dor.pascal@mail.huji.ac.il>
2023-11-09 14:29:39 +0200

    Add problem

--- Medium/Arrays/eliminate-maximum-number-of-monsters_1921.cpp
+++ Medium/Arrays/eliminate-maximum-number-of-monsters_1921.cpp
@@ -52,4 +52,54 @@ class Solution {
         }
 
         // If we've processed all monsters without returning, we can eliminate
-   
+        // all of them.
+        return dist.size();
+    }
+};
+
+
+
+
+/*
+https://leetcode.com/problems/eliminate-maximum-number-of-monsters/submissions/1093489124?envType=daily-question&envId=2023-11-07
+
+Runtime
+Details
+111ms
+Beats 54.67%of users with C++
+Memory
+Details
+83.26MB
+Beats 80.80%of users with C++
+
+*/
+
+class Solution {
+  public:
+    int eliminateMaximum(std::vector<int> &dist, std::vector<int> &speed) {
+        std::vector<int> arrivalTimes(dist.size());
+
+        // Calculate the arrival time for each monster.
+        for (int i = 0; i < dist.size(); ++i) {
+            // Ceiling of dist/speed to get the arrival time
+            arrivalTimes[i] = (dist[i] + speed[i] - 1) / speed[i];
+        }
+
+        // Sort the arrival times.
+        std::sort(arrivalTimes.begin(), arrivalTimes.end());
+
+        // Try to eliminate each monster one by one.
+        for (int i = 0; i < arrivalTimes.size(); ++i) {
+            // If a monster arrives before or when we are eliminating, we can't
+            // eliminate all.
+            if (arrivalTimes[i] <= i) {
+                return i; // The number of monsters eliminated before one
+                          // reaches us.
+            }
+        }
+
+        // If we've processed all monsters without returning, we can eliminate
+        // all of them.
+        return arrivalTimes.size();
+    }
+};
